560b2bff90ca9cffa3bc229604e10baf2644f830
hyunjun <hyunjun306@gmail.com>
2023-05-20 14:18:19 -0400

    added app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+function randomRGB() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+const letters = document.querySelectorAll('.letters');
+const intervalId = setInterval(function () {
+    for (let letter of letters) {
+        letter.style.document = randomRGB();
+    }
+}, 2000);
